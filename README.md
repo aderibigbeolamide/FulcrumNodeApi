03b2b1201c1df14576d6812464378a9793b19bf0
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2024-05-28 13:44:57 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # FulcrumNodeApi
+
+# Here is how you can Test the API
+
+# 1. Clone the repository
+# 2. Run `npm install`
+# 3. Run `npm start`
+# 4. Open Postman and test the API
+
+# Here is the list of the API endpoints
+
+Create todos
+ https://your-service-name.onrender.com/todos (POST)
+
+Get all todos
+    https://your-service-name.onrender.com/todos (GET)
+
+Get a todo
+    https://your-service-name.onrender.com/todos/:id (GET)
+
+Update a todo
+    https://your-service-name.onrender.com/todos/:id (PUT)
+
+Delete a todo
+    https://your-service-name.onrender.com/todos/:id (DELETE)
+
+
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the To-Do List API');
+});
+
+
+app.post('/todos', (req, res) => {
+    const { text } = req.body;
+    const newTodo = { id: uuidv4(), text, isCompleted: false };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        res.json(todo);
+    } else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
+
+
+app.put('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const { text, isCompleted } = req.body;
+    const todo = todos.find(todo => todo.id === id);
+
+    if (todo) {
+        if (text !== undefined) todo.text = text;
+        if (isCompleted !== undefined) todo.isCompleted = isCompleted;
+        res.json(todo);
+    } else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
+
+
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter(todo => todo.id !== id);
+    res.status(204).send();
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
